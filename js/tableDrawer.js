83c4706986d0ae3c137c9e58e02bb6da90f703ec
lorderikir <lorderikir@github.com>
2016-10-17 13:40:07 +1100

    updating with python file and smarter js

--- js/tableDrawer.js
+++ js/tableDrawer.js
@@ -1,8 +1,10 @@
-function addTable(semesters, units) {
+function drawTable(startingYear, semesters, units) {
+    var startingYear = parseInt(startingYear);
     var myTableDiv = document.getElementById("tableLoc");
 
     var table = document.createElement('TABLE');
     table.setAttribute('class','pure-table pure-table-horizontal')
+    table.setAttribute('id','unitTable')
     table.border = '1';
 
     var tableHead = document.createElement('THEAD');
@@ -29,17 +31,33 @@ function addTable(semesters, units) {
         var tr = document.createElement('TR');
         tableBody.appendChild(tr);
 
+        if (i % 2 === 0){
+          var currentSem = (startingYear + i / 2) + " A"
+        } else {
+          var currentSem = (startingYear + Math.floor(i/2)) + " B"
+        }
+        console.log(currentSem)
         for (var j = 0; j < units+1; j++) {
             if(j > 0) {
               var td = document.createElement('TD');
-              td.appendChild(document.createTextNode("Sesmester " + (i+1) + ", Unit " + j));
               tr.appendChild(td);
             } else {
               var td = document.createElement('TD');
-              td.appendChild(document.createTextNode("Semester " + j));
+              td.appendChild(document.createTextNode(currentSem));
               tr.appendChild(td);
             }
         }
     }
     myTableDiv.appendChild(table);
+}
+
+function submitLink(){
+  var element =  document.getElementById('unitTable');
+  if (typeof(element) != 'undefined' && element != null)
+  {
+    if(element) element.parentNode.removeChild(element);
+  }
+  var startingYear = document.getElementById('startingYear').value;
+  var startingYear = document.getElementById('startingYear').value;
+  drawTable(startingYear,8,4)
 }
