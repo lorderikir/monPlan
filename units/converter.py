44fbc5ce3842301206b58c948c285f5316306b1b
lorderikir <lorderikir@github.com>
2016-10-18 14:07:37 +1100

    added db files, added python script which compiles txt files into one huge database

---
+++ units/converter.py
@@ -0,0 +1,34 @@
+import csv
+
+def convsubtoarray(fileName,faculty):
+    file = open(fileName,'r')
+    array=[]
+    for line in file:
+        record=line.strip()
+        record = record.split(' ',1)
+        unitCode=str(record[0])
+        unitName=record[1]
+        pair=[unitCode,unitName,faculty]
+        array.append(pair)
+    return array
+
+import csv
+
+
+
+def toCSV(array):
+    CSVfileName = 'db.csv'
+    fl = open(CSVfileName, 'w')
+
+    writer = csv.writer(fl)
+    for values in array:
+        writer.writerow(values)
+    fl.close()
+
+faculties = ['ada','arts','buseco','edu','eng','it','law','med','pha','sci']
+newarray = []
+for f in faculties:
+    fileName = f + '.txt'
+    array = convsubtoarray(fileName,f)
+    newarray += array
+toCSV(newarray)
