f4040d46fa20fc9f2eb3181a1d97c1f03ca737b5
lorderikir <lorderikir@github.com>
2016-10-18 13:24:41 +1100

    added

--- unitsTxt/converter.py
+++ unitsTxt/converter.py
@@ -3,10 +3,15 @@ import csv
 def convsubtoarray(fileName,faculty, outputFile):
     file = open(fileName,'r')
     for line in file:
-        subdb=[]
-        record=line.strip().split('\t')
+        array=[]
+        record=line.strip()
+        record = record.split(' ',1)
+        print(record)
         unitCode=str(record[0])
-        unitName=record[1].split(',')
-        for location in suburb:
-            pair=[unitCode,unitName] #appends record into array
-            array.append(pair)
+        unitName=record[1]
+        pair=[unitCode,unitName]
+        array.append([]) #appends record into array
+        array[-1].append(pair)
+    print(array)
+
+convsubtoarray('ada.txt','','')
