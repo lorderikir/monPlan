c6015bffe9dcc2eef523283b8277e41db56f04b8
lorderikir <lorderikir@github.com>
2016-10-18 15:59:04 +1100

    rebranding from mCourse -> monPlan

--- units/converter.py
+++ units/converter.py
@@ -32,4 +32,24 @@ for f in faculties:
     fileName = f + '.txt'
     array = convsubtoarray(fileName,f)
     newarray += array
+newarray = quicksort(newarray)
 toCSV(newarray)
+
+def partition(array, begin, end):
+    pivot = begin
+    for i in xrange(begin+1, end+1):
+        if array[i] <= array[begin]:
+            pivot += 1
+            array[i], array[pivot] = array[pivot], array[i]
+    array[pivot], array[begin] = array[begin], array[pivot]
+    return pivot
+
+
+def quicksort(array, begin=0, end=None):
+    if end is None:
+        end = len(array) - 1
+    if begin >= end:
+        return
+    pivot = partition(array, begin, end)
+    quicksort(array, begin, pivot-1)
+    quicksort(array, pivot+1, end)
