bdb21e67e39ee9e29055225a485915bfe980334d
lorderikir <lorderikir@github.com>
2016-11-25 12:47:43 +1100

    update site design, moved search so users can search degree by unit and code, semantic-ui's search function now uses element id unlike class previously

--- src/js/search.js
+++ src/js/search.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
             content[i].description = content[i]["UnitName"];
         }
 
-        $(".ui.search").search({
+        $("#unitSearch").search({
             source: content,
             searchFields: [
                 "title", "description"
@@ -33,10 +33,31 @@ $(document).ready(function() {
     $.ajax({
         url: "data/courses/bachelors.json"
     }).done(function(content) {
-        content.map(function(ele) {
-            var option = $("<option></option>");
-            option.text(ele.courseName);
-            $("#courseSearch").append(option);
-        });
+
+      for(var i = 0; i < content.length; i++) {
+          content[i].title = content[i]["courseCode"];
+          content[i].description = content[i]["courseName"];
+      }
+
+      $("#courseSearch").search({
+          source: content,
+          searchFields: [
+              "title", "description"
+          ],
+          searchFullText: false,
+          maxResults: 5,
+          onSelect: function(result) {
+              if(typeof courseStructure !== "undefined") {
+                  var unit = new Unit(result.UnitCode, result.UnitName);
+
+                  courseStructure.promptUserToAddUnit(unit, function() {
+                      addUnitMessage.classList.add("hide");
+                  });
+
+                  addUnitMessage.classList.remove("hide");
+              }
+          }
+      });
+
     });
 });
